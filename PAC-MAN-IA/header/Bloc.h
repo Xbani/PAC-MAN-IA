fa95256b8f44ddbff8439c1bfc215c2267ababd1
personnedu13 <fabienpupetto@gmail.com>
2020-09-01 18:44:52 +0200

    repush bloc
    Fail de ma part

--- PAC-MAN-IA/header/Bloc.h
+++ PAC-MAN-IA/header/Bloc.h
@@ -1,11 +1,18 @@
 #pragma once
 
-class Bloc
+#include <memory>
+
+#include "Controller.h"
+#include "Entity.h"
+#include "EntityBloc.h"
+
+class Bloc : public Entity
 {
 public:
     enum BlocType {
+        WALL,
         EMPTY,
-        DOT,
+        POINT,
         GUM
     };
 
@@ -17,9 +24,9 @@ public:
     Bloc(const Bloc& blocIn);
     ~Bloc();
 
-    Bloc(BlocType typeIn);
+    Bloc(BlocType typeIn, std::shared_ptr<Entity> contenuIn);
 
-    void onPacmanPresence();
+    Bloc::BlocType onPacmanPresence();
 
     Bloc& operator=(const Bloc& blocIn);
 };
