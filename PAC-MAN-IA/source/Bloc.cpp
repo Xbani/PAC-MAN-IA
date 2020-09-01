e3c31f66e756e631b8b40b5b96b3c3aaa2984ced
personnedu13 <fabienpupetto@gmail.com>
2020-09-01 18:41:09 +0200

    Bloc fait

--- PAC-MAN-IA/source/Bloc.cpp
+++ PAC-MAN-IA/source/Bloc.cpp
@@ -1,1 +1,38 @@
 #include "Bloc.h"
+
+Bloc::Bloc()
+{
+	type = EMPTY;
+}
+
+
+
+Bloc::Bloc(const Bloc& blocIn)
+{
+	type = blocIn.type;
+}
+
+Bloc::~Bloc()
+{
+}
+
+Bloc::Bloc(BlocType typeIn, std::shared_ptr<Entity> contenuIn)
+{
+	type = typeIn;
+}
+
+Bloc::BlocType Bloc::onPacmanPresence()
+{
+	if (type != EMPTY && type != WALL) {
+		BlocType typeOut = type;
+		type = EMPTY;
+		return typeOut;
+	} else {
+		return type;
+	}
+}
+
+Bloc& Bloc::operator=(const Bloc& blocIn)
+{
+	type = blocIn.type;
+}
