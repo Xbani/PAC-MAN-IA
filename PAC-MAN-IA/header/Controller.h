e594623ee87f634995eb17754d0a3132860a7825
Oscar Bonduelle <oscar.bonduelle@hotmail.fr>
2020-09-01 18:42:03 +0200

    Update Controller

--- PAC-MAN-IA/header/Controller.h
+++ PAC-MAN-IA/header/Controller.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <SFML/Window.hpp>
 
 #include "Bloc.h"
 
@@ -7,15 +8,25 @@ class Controller
 {
 protected:
     std::vector<std::vector<Bloc>> grille;
+
     int largeur;
     int hauteur;
 
+    static const int cellWidth = 32;
+    static const int cellHeight = 32;
+
+    sf::Window window(sf::VideoMode(800, 600), "My window");
+
 public:
     Controller();
-    Controller(const Controller& controlelrIn);
+    Controller(const Controller& controllerIn);
     ~Controller();
 
     Controller(int largeurIn, int hauteurIn);
 
     Controller& operator=(const Controller& controllerIn);
+
+private:
+    void makeGrid();
+    void makeGrid(int rowIn, int columnIn);
 };
--- PAC-MAN-IA/source/Controller.cpp
+++ PAC-MAN-IA/source/Controller.cpp
@@ -1,1 +1,33 @@
 #include "Controller.h"
+
+Controller::Controller() {
+
+}
+
+Controller::Controller(int largeurIn, int hauteurIn) {
+	largeur = largeurIn;
+	hauteur = hauteurIn;
+	makeGrid();
+}
+
+void Controller::makeGrid() {
+	makeGrid(28, 28);
+}
+
+void Controller::makeGrid(int rowIn, int columnIn) {
+	grille = std::vector<std::vector<Bloc>>();
+
+	for (int x = 0; x < rowIn; ++x) {
+
+		std::vector<Bloc> vectorRow;
+		grille.insert(grille.end(), vectorRow);
+
+		for (int y = 0; y < columnIn; ++y) {
+			vectorRow.insert(vectorRow.end(), Bloc());
+		}
+	}
+}
+
+void Controller :: initializeWindow() {
+	window = sf::Window(sf::VideoMode(largeur*cellWidth, hauteur*cellHeight), "PAC-MAN");
+}
